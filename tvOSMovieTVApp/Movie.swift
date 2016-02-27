8e7565e7f0f9c2b496a415b989125004ea79fc37
Karlo Pagtakhan <kidap@yahoo.com>
2016-02-27 12:04:42 -0500

    first commit

---
+++ tvOSMovieTVApp/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  tvOSMovieTVApp
+//
+//  Created by Karlo Pagtakhan on 02/24/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
---
+++ tvOSMovieTVApp/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  tvOSMovieTVApp
+//
+//  Created by Karlo Pagtakhan on 02/25/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
